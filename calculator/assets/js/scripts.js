b0c2bd8ac00ca1d38e616d5854cb46bbeb490c45
Breland Norwood <brelandnorwood@gmail.com>
2023-02-01 16:36:02 +0000

    added calculator

---
+++ calculator/assets/js/scripts.js
@@ -0,0 +1,75 @@
+function calculate() {
+    
+    /* Make sure that the form is valid */
+    if ($( "#myform" ).valid()) {
+        
+        /* get the operands from the form */
+        var operand1 = document.getElementById("Operand1").value;
+        var operand2 = document.getElementById("Operand2").value;
+        
+        
+        /* convert the operands from string to floating point */
+        var operand1fp = parseFloat (operand1);
+        var operand2fp = parseFloat (operand2);
+        
+        
+        /* figure out which operator was checked and place the value in operator */
+        var operator;
+        if (document.getElementById("AddOperator").checked) {
+            operator = document.getElementById("AddOperator").value;
+        }
+        if (document.getElementById("SubOperator").checked) {
+            operator = document.getElementById("SubOperator").value;
+        }
+        if (document.getElementById("MultOperator").checked) {
+            operator = document.getElementById("MultOperator").value;
+        }
+        if (document.getElementById("DivOperator").checked) {
+            operator = document.getElementById("DivOperator").value;
+        }
+        var result;
+        
+        /* if the operator was "Add" then add the numbers */
+        if (operator == "Add") {
+            result = operand1fp + operand2fp;
+        }
+ 
+        /* if the operator was "Sub" then subtract the numbers*/
+        if (operator == "Sub") {
+            result = operand1fp - operand2fp;
+        }
+
+        /* if operator was "Mult" the multiply the numbers */
+        if (operator == "Mult") {
+            result = operand1fp * operand2fp;
+        }
+        /* if operator was "Div" the divide the numbers */
+        if (operator == "Div") {
+            result = operand1fp / operand2fp;
+        }
+        /* convert the result to a string and display it */
+        document.getElementById("Result").innerHTML = result.toString();
+    }
+}
+
+function clearform() {
+    
+    /* Set all of the form values to blank or false */
+    document.getElementById("Operand1").value = "";
+    document.getElementById("Operand2").value = "";
+    
+    document.getElementById("Operand1Error").innerHTML = "";
+    document.getElementById("Operand2Error").innerHTML = "";
+    
+    document.getElementById("AddOperator").checked = false;
+    document.getElementById("SubOperator").checked = false;
+    document.getElementById("MultOperator").checked = false;
+    document.getElementById("DivOperator").checked = false;
+    document.getElementById("OperatorError").innerHTML = "";
+    document.getElementById("Result").innerHTML = "";
+}
+
+/* Form Validation */
+$( "#myform" ).validate({
+ 
+});
